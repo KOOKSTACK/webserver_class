1d15d2032f64053a1e944af1a02875ad952b40e3
Junyong <jypark93@kookmin.ac.kr>
2023-05-17 04:13:03 +0900

    Feat : Prob3 : 댓글 추천 기능 구현 완 댓글 추천을 위해 댓글 모델에 추천 기능을 추가하여 url 매핑 이후 detail에서 추천 버튼을 눌렀을 시 댓글 추천 가능하게 하며 자추는 불가능

--- pybo/views/vote_views.py
+++ pybo/views/vote_views.py
@@ -1,7 +1,8 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect
-from ..models import Question, Answer
+from ..models import Question, Answer,Comment
+
 @login_required(login_url='common:login')
 def vote_question(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -19,3 +20,13 @@ def vote_answer(request, answer_id):
     else:
         answer.voter.add(request.user)
     return redirect('pybo:detail', question_id=answer.question.id)
+
+
+@login_required(login_url='common:login')
+def vote_comment(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if request.user == comment.author:
+        messages.error(request, '본인이 작성한 댓글은 추천할 수 없습니다.')
+    else:
+        comment.voter.add(request.user)
+    return redirect('pybo:detail', question_id= comment.question.id)
