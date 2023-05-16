c6d3eb658a47643803a68cb60012de7f099a5d1c
Junyong <dak_kiwon@JunYong-MacBook-Pro.local>
2023-05-17 00:17:52 +0900

    Feat : prob2 댓글 페이징 기능 추가 Comment 페이징 기능 구현을 위해 Detail 페이지에 대한 GET 요청이 들어온 경우 해당 페이지에 대한 오브젝트 리스트를 만들고 10개씩 표시되도록 페이징

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -2,6 +2,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 from ..models import Question
+from ..models import Comment
 
 
 def index(request):
@@ -22,5 +23,18 @@ def detail(request, question_id):
     """
     # question = Question.objects. get(id = question_id)
     question = get_object_or_404(Question, pk = question_id)
-    context = {'question': question}
+    
+    comment_page = request.GET.get("page", "1") # 1번 page에 있는 모든 데이터
+    comment_list = Comment.objects.order_by('-create_date') #-- 페이지에 잇는 모든 답변 객체리스트
+    # print("comment lsit data is : " , comment_list) 
+    paginator = Paginator(comment_list, 10) #-- 10개씩 
+    page_obj = paginator.get_page(comment_page)
+    # print("comment obj data is : " , page_obj) 흐음...
+    """
+    comment obj data is :  <Page 1 of 2>
+    [16/May/2023 23:54:42] "GET /pybo/1?page=1 HTTP/1.1" 200 14505
+    comment obj data is :  <Page 2 of 2>
+    """
+    context = {'question': question, 'comment_list' : page_obj, }    
+    #-- 질문에 대한 정보, 페이지에 대한 정보를 넘겨준다.
     return render(request, 'pybo/question_detail.html', context)
